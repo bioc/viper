c476743f1d0ebbb92323d957ec308481ea424dad
Mariano Javier Alvarez <malvarez@c2b2.columbia.edu>
2015-06-10 02:38:59 +0000

    Bug fixed in aracne2regulon function
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/viper@104768 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/aracne.r
+++ R/aracne.r
@@ -53,6 +53,7 @@ aracne2regulon <- function(afile, eset, gene = FALSE, format=c("adj", "3col"), v
     }
     if (verbose) message("Generating the regulon objects...")
     tmp <- aracne[!is.na(aracne$mi), ]
+    tmp <- tmp[rowSums(matrix(as.matrix(tmp[, 1:2]) %in% rownames(dset), nrow(tmp), 2))==2, ]
     aracne <- tapply(1:nrow(tmp), tmp$tf, function(pos, tmp) {
         tfmode <- rep(0, length(pos))
         names(tfmode) <- tmp$target[pos]
